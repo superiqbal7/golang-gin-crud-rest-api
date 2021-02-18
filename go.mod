de2ac10515b32c9faedcb2bc990421179c740e4b
Rahim Iqbal <superiqu@gmail.com>
2021-02-19 00:12:12 +0600

    created server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pragmaticreviews/golang-gin-poc
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pragmaticreviews/golang-gin-poc/controller"
+	"github.com/pragmaticreviews/golang-gin-poc/service"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+
+	// server.GET("/test", func(ctx *gin.Context) {
+	// 	ctx.JSON(200, gin.H{
+	// 		"message": "test!",
+	// 	})
+	// })
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.Run(":8080")
+}
