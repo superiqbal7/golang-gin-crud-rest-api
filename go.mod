b6ce4fecb15773c9884acf572a91b819ada845fa
Rahim Iqbal <superiqu@gmail.com>
2021-02-19 12:07:33 +0600

    added middlewares: auth, logging, debug

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pragmaticreviews/golang-gin-poc
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/tpkeeper/gin-dump v1.0.0 // indirect
+)
--- server.go
+++ server.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/pragmaticreviews/golang-gin-poc/controller"
+	"github.com/pragmaticreviews/golang-gin-poc/middlewares"
 	"github.com/pragmaticreviews/golang-gin-poc/service"
+	gindump "github.com/tpkeeper/gin-dump"
 )
 
 var (
@@ -11,14 +16,24 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
-func main() {
-	server := gin.Default()
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
 
+func main() {
+	//server := gin.Default()
 	// server.GET("/test", func(ctx *gin.Context) {
 	// 	ctx.JSON(200, gin.H{
 	// 		"message": "test!",
 	// 	})
 	// })
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
 	})
